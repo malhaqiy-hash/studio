f1c62173c14c8bfb2361d785af24871c223b7c80
Muhammad Anas <m.alhaqiy@gmail.com>
2026-06-19 12:17:44 +0000

    Try fixing this error: `Runtime ReferenceError: ScrollArea is not define

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -10,6 +10,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { 
   Instagram, 
   Linkedin, 
