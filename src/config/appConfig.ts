8aa823dc52523e3ca0db38adbebd24cbbcb99ab2
malhaqiy-hash <m.alhaqiy@gmail.com>
2026-06-28 20:12:20 +0700

    chore(config): add central app config (APP_NAME/BRAND)

---
+++ src/config/appConfig.ts
@@ -0,0 +1,9 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? 'Koolink';
+export const APP_SHORT = process.env.NEXT_PUBLIC_APP_SHORT ?? 'Koolink';
+export const SUPPORT_EMAIL = process.env.NEXT_PUBLIC_SUPPORT_EMAIL ?? 'support@koolink.example';
+
+export const BRAND = {
+  name: APP_NAME,
+  short: APP_SHORT,
+  supportEmail: SUPPORT_EMAIL,
+};
